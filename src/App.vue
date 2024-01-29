82887147d6887dad49d29db7271e301cfb10637d
fideline <fideline.bazana-ntomo@charlie-solutions.com>
2024-01-29 16:48:41 +0100

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-7xl text-secondary text-center pt-80">LIMOGES</h1>
+  </div>
+  <!--<HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+</style>
