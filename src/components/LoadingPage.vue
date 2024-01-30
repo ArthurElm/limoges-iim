3a35e92dab65e4a8dfdd81ae5bdc15feb6e34fc0
Jona-B <78787130+Jona-B@users.noreply.github.com>
2024-01-30 16:55:54 +0100

    Animation Limoges

--- src/components/LoadingPage.vue
+++ src/components/LoadingPage.vue
@@ -11,14 +11,14 @@
         <!-- Centered Limoges map behind text -->
         <div class="centered-container limoges-map-container">
             <svg xmlns="http://www.w3.org/2000/svg" width="452" height="531" viewBox="0 0 452 531" fill="none">
-                <path d="M248.877 456.829L219.066 433.455L193.321 432.94L167.704 406.073L105.038 396.78L90.7421 375.396L99.0952 337.262L24.2173 237.02L38.2049 201.04L3 184.59L9.93806 173.624L47.8959 179.682L81.6857 161.284L128.323 204.532L154.308 248.973L235.361 288.11L281.106 262.908L305.985 232.343L306.122 181.372L323.069 146.122L321.216 120.396L338.205 100.84L321.148 39.5891L335.024 3L417.037 6.83568L422.466 31.8577L449 57.7293L425.407 113.771L436.891 149.365L425.682 155.123L414.301 203.983L420.45 240.572L412.423 274.656L425.562 295.816L350.109 362.542L392.998 407.729L400.305 444.884L385.614 493.032L374.628 499.193L343.557 483.799L342.382 509.138L329.732 526L323.026 502.643L310.968 501.416L288.438 489.471L277.126 442.07L265.163 437.668L248.877 456.829Z" stroke="#00368D" stroke-width="4"/>
+                <path d="M248.877 456.829L219.066 433.455L193.321 432.94L167.704 406.073L105.038 396.78L90.7421 375.396L99.0952 337.262L24.2173 237.02L38.2049 201.04L3 184.59L9.93806 173.624L47.8959 179.682L81.6857 161.284L128.323 204.532L154.308 248.973L235.361 288.11L281.106 262.908L305.985 232.343L306.122 181.372L323.069 146.122L321.216 120.396L338.205 100.84L321.148 39.5891L335.024 3L417.037 6.83568L422.466 31.8577L449 57.7293L425.407 113.771L436.891 149.365L425.682 155.123L414.301 203.983L420.45 240.572L412.423 274.656L425.562 295.816L350.109 362.542L392.998 407.729L400.305 444.884L385.614 493.032L374.628 499.193L343.557 483.799L342.382 509.138L329.732 526L323.026 502.643L310.968 501.416L288.438 489.471L277.126 442.07L265.163 437.668L248.877 456.829Z" stroke="#00368D" stroke-width="6" stroke-dasharray="2000" stroke-dashoffset="2000"/>
             </svg>
         </div>
     </div>
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Bungee&family=Bungee+Inline&display=swap');
 
 /* Texte */
 .centered-container {
@@ -69,7 +69,7 @@
         background-position: 100% 0;
     }
     100% {
-        background-position: -100% 0;
+        background-position: 0 100%;
     }
 }
 
@@ -95,4 +95,15 @@
     justify-content: center;
     z-index: 1; /* Adjust the z-index to position it behind your text */
 }
+
+@keyframes fillStrokeAnimation {
+    to {
+        stroke-dashoffset: 0;
+    }
+}
+
+.limoges-map-container path {
+    animation: fillStrokeAnimation 2s ease forwards;
+}
+
 </style>
